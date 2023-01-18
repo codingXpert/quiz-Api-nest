d7edcaddbab656c22ef2c64e5e8b01224f2575e3
Vivek Raj <vivek.raj@brainvire.com>
2023-01-18 19:00:23 +0530

    getting all the questions related to a quiz

--- src/modules/quiz/quiz.controller.ts
+++ src/modules/quiz/quiz.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Param, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { CreateQuizDto } from "./dto/create-quiz.dto";
 import { QuizService } from "./quiz.service";
 
@@ -17,5 +17,10 @@ export class QuizController {
    CreateQuiz(@Body() quizDto: CreateQuizDto){
     return this.quizService.createNewQuiz(quizDto)
    }
+
+   @Get(':id')
+   async getQuizById(@Param('id') id: number){
+        return await this.quizService.getQuizById(+id)
+    }
     
 }
