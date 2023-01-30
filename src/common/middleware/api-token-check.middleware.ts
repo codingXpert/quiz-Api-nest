36e925f770395cb5e9674fabd3a1f877b2a3ec6b
Vivek Raj <vivek.raj@brainvire.com>
2023-01-30 17:50:14 +0530

    custom exception using HttpException

--- src/common/middleware/api-token-check.middleware.ts
+++ src/common/middleware/api-token-check.middleware.ts
@@ -1,10 +1,13 @@
-import { BadRequestException, NestMiddleware } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 export class ApiTokenCheckMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     if (req.headers['api-token'] !== 'my-token')    // In postman, run http://localhost:3000 & from header pass api-token: my-token'
-      throw new BadRequestException('The token does not match');
+      throw new HttpException(
+        'My Exception',
+        HttpStatus.PAYMENT_REQUIRED
+      )
     next();
   }
 }
